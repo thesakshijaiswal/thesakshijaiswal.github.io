36b3f5488ec6a95d9e56d06157190867d31f260f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-07-29 22:02:35 +0530

    chore: initialize Next.js portfolio project with Typescript, Tailwind and App Router

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-pink-500 text-5xl font-semibold">Sakshi Jaiswal</h1>
+    </div>
+  );
+}
