5e90c63671d8abb4d021db404ad184681ace9072
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-08-04 21:25:15 +0530

    feat: create hero section with gradient and intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Hero from "@/sections/Hero";
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-pink-500 text-5xl font-semibold">Sakshi Jaiswal</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
