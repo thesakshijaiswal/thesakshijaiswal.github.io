f08a058b36d00ccd87b24aa8e173c6f459149cf7
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-08-04 21:17:53 +0530

    chore: initialize Next.js portfolio project with Typescript, Tailwind and App Router

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Sakshi Jaiswal",
+  description: "My personal portfolio website!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
