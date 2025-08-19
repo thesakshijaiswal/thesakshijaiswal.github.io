48c463b22bc2ba96eda2db004dcde5471b1e3c99
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-08-19 21:24:43 +0530

    feat: add LogoFlipDeck component for animated logo display in SkillsPage

--- src/app/tabs/skills/page.tsx
+++ src/app/tabs/skills/page.tsx
@@ -1,8 +1,55 @@
+import { FaReact } from "react-icons/fa";
+import {
+  SiNextdotjs,
+  SiJavascript,
+  SiTypescript,
+  SiTailwindcss,
+  SiHtml5,
+} from "react-icons/si";
+import { LogoFlipDeck, LogoItem } from "@/components/LogoFlipAnimation";
+import LogoBox from "@/components/LogoBox";
+
 export default function SkillsPage() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Top Skills</h1>
-      <p className="mt-4 text-gray-500">This is the Skill section content.</p>
+    <div className="relative flex">
+      <div className="sm:ml-8">
+        <div className="flex mb-10 items-center gap-4">
+          <LogoBox className="place-content-center">
+            <LogoFlipDeck
+              items={[
+                <LogoItem key="react" className="bg-sky-400 text-white">
+                  <FaReact />
+                </LogoItem>,
+                <LogoItem
+                  key="nextjs"
+                  className="bg-black dark:bg-white dark:text-neutral-900 text-white"
+                >
+                  <SiNextdotjs />
+                </LogoItem>,
+                <LogoItem
+                  key="javascript"
+                  className="bg-yellow-300 text-neutral-900 dark:text-white"
+                >
+                  <SiJavascript />
+                </LogoItem>,
+                <LogoItem
+                  key="typescript"
+                  className="dark:text-blue-400 text-neutral-900 bg-blue-400 dark:bg-white"
+                >
+                  <SiTypescript />
+                </LogoItem>,
+                <LogoItem key="tailwind" className="bg-sky-500 text-white">
+                  <SiTailwindcss />
+                </LogoItem>,
+                <LogoItem key="html" className="bg-orange-500 text-white">
+                  <SiHtml5 />
+                </LogoItem>,
+              ]}
+            />
+          </LogoBox>
+          <h1 className="text-2xl font-bold">My SuperPowers</h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/LogoFlipAnimation.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const DELAY_IN_MS = 2500;
+const TRANSITION_DURATION_IN_SECS = 1.5;
+
+interface LogoFlipDeckProps {
+  items: React.ReactNode[];
+}
+
+export const LogoFlipDeck: React.FC<LogoFlipDeckProps> = ({ items }) => {
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setIndex((pv) => pv + 1);
+    }, DELAY_IN_MS);
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        transform: "rotateY(-20deg)",
+        transformStyle: "preserve-3d",
+        perspective: "1000px",
+      }}
+      className="relative h-20 w-16 shrink-0"
+    >
+      <AnimatePresence mode="sync">
+        <motion.div
+          style={{
+            y: "-50%",
+            x: "-50%",
+            clipPath: "polygon(0 0, 100% 0, 100% 50%, 0 50%)",
+            zIndex: -index,
+            backfaceVisibility: "hidden",
+          }}
+          key={index}
+          transition={{
+            duration: TRANSITION_DURATION_IN_SECS,
+            ease: "easeInOut",
+          }}
+          initial={{ rotateX: "0deg" }}
+          animate={{ rotateX: "0deg" }}
+          exit={{ rotateX: "-180deg" }}
+          className="absolute left-1/2 top-1/2"
+        >
+          {items[index % items.length]}
+        </motion.div>
+
+        <motion.div
+          style={{
+            y: "-50%",
+            x: "-50%",
+            clipPath: "polygon(0 50%, 100% 50%, 100% 100%, 0 100%)",
+            zIndex: index,
+            backfaceVisibility: "hidden",
+          }}
+          key={(index + 1) * 2}
+          initial={{ rotateX: "180deg" }}
+          animate={{ rotateX: "0deg" }}
+          exit={{ rotateX: "0deg" }}
+          transition={{
+            duration: TRANSITION_DURATION_IN_SECS,
+            ease: "easeInOut",
+          }}
+          className="absolute left-1/2 top-1/2"
+        >
+          {items[index % items.length]}
+        </motion.div>
+      </AnimatePresence>
+
+      <hr
+        style={{
+          transform: "translateZ(1px)",
+        }}
+        className="absolute left-0 right-0 top-1/2 z-[999999999] -translate-y-1/2 border-t-2 dark:border-neutral-800 border-white"
+      />
+    </div>
+  );
+};
+
+interface LogoItemProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const LogoItem: React.FC<LogoItemProps> = ({
+  children,
+  className = "",
+}) => {
+  return (
+    <div
+      className={`grid size-16 place-content-center rounded-lg text-5xl ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
