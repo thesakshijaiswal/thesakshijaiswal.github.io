97fc85a01c868112b383dbd4e2432c30e53ae58a
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-08-23 22:15:31 +0530

    feat: enhance SkillsPage with tags for HTML, CSS, JavaScript, and TypeScript

--- src/app/tabs/skills/page.tsx
+++ src/app/tabs/skills/page.tsx
@@ -1,9 +1,11 @@
 import SkillFlip from "@/components/SkillFlip";
 import LogoBox from "@/components/LogoBox";
 import SkillCard from "@/components/SkillCard";
+import Tag from "@/components/Tag";
 import { LuBrainCircuit } from "react-icons/lu";
-import { FaLaptopCode } from "react-icons/fa";
+import { FaLaptopCode, FaHtml5, FaCss3Alt } from "react-icons/fa";
 import { MdAutoGraph } from "react-icons/md";
+import { SiJavascript, SiTypescript } from "react-icons/si";
 export default function SkillsPage() {
   return (
     <div className="relative flex">
@@ -32,6 +34,43 @@ export default function SkillsPage() {
             />
             <SkillCard icon={<FaLaptopCode />} label="Responsive Design" />
           </div>
+        </div>
+
+        <div className="space-y-4 mt-6">
+          <h4 className="flex items-center gap-2 text-lg font-bold">
+            Crafting Stunning Interfaces
+          </h4>
+
+          <div className="flex gap-2 flex-wrap">
+            <Tag
+              icon={<FaHtml5 size={17} className="text-orange-500" />}
+              label="HTML"
+              className="text-xs"
+            />
+            <Tag
+              icon={<FaCss3Alt size={17} className="text-blue-500" />}
+              label="CSS"
+              className="text-xs"
+            />
+            <Tag
+              icon={<SiJavascript size={13} className="text-yellow-400" />}
+              label="Javascript"
+              className="text-xs"
+            />
+            <Tag
+              icon={<SiTypescript size={13} className="text-blue-500" />}
+              label="Typescript"
+              className="text-xs"
+            />
+          </div>
+
+          <h4 className="flex items-center gap-2 text-lg font-bold">
+            Building Robust Backends
+          </h4>
+
+          <h4 className="flex items-center gap-2 text-lg font-bold">
+            Modern Tooling
+          </h4>
         </div>
       </div>
     </div>
