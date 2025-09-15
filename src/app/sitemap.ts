904d2e1bd46950b4cbd5f04004f11ba6b0e980da
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-09-15 20:17:12 +0530

    feat: create programmatic robots txt file and fix sitemap urls in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,40 +1,39 @@
 import type { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://sakshi.is-cool.dev"
   return [
     {
-      url: `${baseUrl}`,
+      url: "https://sakshi.is-cool.dev",
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/tabs/projects`,
+      url: "https://sakshi.is-cool.dev/tabs/projects",
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
      {
-      url: `${baseUrl}/tabs/experience`,
+      url: "https://sakshi.is-cool.dev/tabs/experience",
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/tabs/skills`,
+      url: "https://sakshi.is-cool.dev/tabs/skills",
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: `${baseUrl}/tabs/education`,
+      url: "https://sakshi.is-cool.dev/tabs/education",
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.6,
     },
     {
-      url: `${baseUrl}/tabs/contact`,
+      url: "https://sakshi.is-cool.dev/tabs/contact",
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
