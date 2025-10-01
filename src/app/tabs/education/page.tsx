18f3b918a9b21c732f3e6022dbed1a4f31397ce6
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-10-01 23:11:26 +0530

    refactor: clean up EducationPage component structure and styling

--- src/app/tabs/education/page.tsx
+++ src/app/tabs/education/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import LogoBox from "@/components/LogoBox";
 import { HiAcademicCap } from "react-icons/hi2";
 import { educationData } from "@/data/educationData";
-import WIP from "@/components/WIP";
 
 export const metadata: Metadata = {
   title: "Education",
@@ -13,8 +12,8 @@ export const metadata: Metadata = {
 export default function EducationPage() {
   return (
     <div className="sm:ml-8">
-      <div className="flex mb-10 items-center gap-4">
-        <LogoBox className="place-content-center size-20">
+      <div className="mb-10 flex items-center gap-4">
+        <LogoBox className="size-20 place-content-center">
           <HiAcademicCap className="size-11" />
         </LogoBox>
         <h1 className="text-2xl font-bold">My Academic Path</h1>
@@ -25,18 +24,22 @@ export default function EducationPage() {
       <div className="mt-5">
         {educationData.map((data) => {
           return (
-            <LogoBox key={data.id} className="p-2 mb-5">
-              <div>
-                <h2 className="font-medium">{data.name}</h2>
-                <h2 className="text-gray-700 text-sm dark:text-gray-400">
-                  {data.degree}
-                </h2>
+            <LogoBox key={data.id} className="mb-5 p-4">
+              <div className="space-y-1">
+                <h2 className="text-sm font-medium">{data.name}</h2>
+                <div className="flex flex-col justify-between text-sm text-gray-700 lg:flex-row dark:text-gray-400">
+                  <h2>{data.degree}</h2>
+                  <p className="">{data.location}</p>
+                </div>
+                <div className="flex items-center justify-between text-xs">
+                  <p>{data.score}</p>
+                  <p>{data.duration}</p>
+                </div>
               </div>
             </LogoBox>
           );
         })}
       </div>
-      <WIP />
     </div>
   );
 }
