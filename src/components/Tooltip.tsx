f4deb5a786088a6dbe0c90b2915cdf3873708e85
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-11-03 16:53:38 +0530

    style: make tooltip responsive and hidden on small screen sizes

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -25,7 +25,7 @@ export default function Tooltip({ description, children }: TooltipProps) {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 6 }}
             transition={{ duration: 0.2 }}
-            className="absolute bottom-full mb-2 w-3xs rounded-lg bg-white/90 px-1 py-1.5 text-center shadow-lg backdrop-blur-sm dark:bg-black/80"
+            className="absolute bottom-full mb-2 hidden rounded-lg bg-white/90 px-1 py-1.5 text-center shadow-lg backdrop-blur-sm md:mr-40 md:block md:w-48 lg:mr-36 xl:mr-0 xl:w-56 dark:bg-black/80"
           >
             {description && (
               <p className="mt-1 text-[10px] leading-snug text-gray-700 lg:text-xs dark:text-gray-300">
