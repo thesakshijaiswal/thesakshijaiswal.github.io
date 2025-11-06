42f2d0fcbb3b8b705988ad1f04fdb383e08e2ac0
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-11-06 15:51:57 +0530

    fix: correct import path and improve footer accessibility

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,4 @@
-import { FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
-import { BiLogoGmail } from "react-icons/bi";
-import { PiGithubLogoFill } from "react-icons/pi";
-import { socialsData } from "@/data/SocialsData";
+import { socialsData } from "@/data/socialsData";
 
 export default function Footer() {
   return (
@@ -10,19 +7,24 @@ export default function Footer() {
         <div>
           <div>
             <h4 className="text-2xl font-semibold">Let&apos;s Work Together</h4>
-            <p className="mt-1 text-sm text-gray-400">
+            <p className="mt-1 text-sm text-gray-800 dark:text-gray-400">
               Collabs, roles, or big ideas? Let&apos;s make it happen
             </p>
-            <nav className="mt-3">
-              <ul className="flex items-center gap-3 text-gray-400">
-                {socialsData.map(({ icon: Icon, link, label }) => {
-                  return (
-                    <a key={label} href={link}>
-                      <Icon size={27} />
-                    </a>
-                  );
-                })}
-              </ul>
+            <nav className="mt-3 flex items-center gap-3">
+              {socialsData.map(({ icon: Icon, link, label }) => {
+                return (
+                  <a
+                    className="text-gray-500 transition-all duration-300 hover:-translate-y-1 hover:scale-105 hover:text-gray-800 active:scale-95 sm:text-2xl dark:text-gray-400 dark:hover:text-white"
+                    key={label}
+                    target="_blank"
+                    href={link}
+                    rel="noopener noreferrer"
+                    aria-label={label}
+                  >
+                    <Icon className="size-7" />
+                  </a>
+                );
+              })}
             </nav>
           </div>
         </div>
