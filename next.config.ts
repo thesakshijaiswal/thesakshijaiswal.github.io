86c5c57a5aca4dc182ea6d9a2c5dc3b5b291812f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-11-30 20:30:00 +0530

    fix: revert route changes to fix api key not found error in pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ...(process.env.DEPLOY_TARGET === 'github' && {
-    output: 'export',
-  }),
 };
 
 export default nextConfig;
